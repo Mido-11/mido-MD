548a9cd69e07aae17e67c41e1e14c133086749f8
Mido-11 <96103438+Mido-11@users.noreply.github.com>
2022-11-15 21:31:49 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,27 +7,27 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['918101187835']
-global.OwnerNumber = ['918101187835']
-global.ownertag = ['918101187835']
-global.BotName = "Miku"
-global.packname = "Miku bot"
-global.author = "By: Fantox"
-global.OwnerName = "Fantox"
-global.BotSourceCode = "https://github.com/FantoX001/Miku-MD"
-global.SupportGroupLink = "https://chat.whatsapp.com/JcBzjRIxM5UHfuJbt0KrWt"
+global.Owner = ['249923561274']
+global.OwnerNumber = ['249923561274']
+global.ownertag = ['249923561274']
+global.BotName = "MIDO"
+global.packname = "MIDO bot"
+global.author = "By: MIDO"
+global.OwnerName = "MIDO"
+global.BotSourceCode = "sorry"
+global.SupportGroupLink = "https://chat.whatsapp.com/DW8HUS3CnfxKjM2HcoF6Gy"
 global.sessionName = "session"
 
 
 
-global.prefa = ['-'] // Prefix ---  change it to anythong you want except @//
+global.prefa = ['.'] // Prefix ---  change it to anythong you want except @//
 
 
-global.location = "West Bengal, India"
+global.location = "khartoum, sudan"
 global.reactmoji = "❤️"
 global.themeemoji = "💖"
 global.vidmenu = { url: 'https://c.tenor.com/YGuLegQWubwAAAPo/miku-nakano-gotoubun-no-hanayome.mp4' }
-global.websitex = "https://github.com/FantoX001"
+global.websitex = "https://google.com"
 global.lolhuman = "KaysaS"
 
 
